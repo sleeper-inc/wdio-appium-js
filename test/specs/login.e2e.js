f6f00fbe1fd99859350d82a6a18d0e5f6dc292fa
Dika <fransiskus.setiawan@stockbit.com>
2025-06-06 23:15:16 +0700

    test: add success login test

--- test/specs/login.e2e.js
+++ test/specs/login.e2e.js
@@ -1,5 +1,13 @@
 describe('Login Feature', () => {
     it("Success Login", async () =>  {
-        console.log("Login Test: Success Login");
+        const fieldUsername = await $('~test-Username');
+        const fieldPassword = await $('~test-Password');
+        const btnSubmit = await $('~test-LOGIN');
+        const textProductPage = await $('~test-Cart drop zone');
+
+        await fieldUsername.setValue('standard_user');
+        await fieldPassword.setValue('secret_sauce');
+        await btnSubmit.click();
+        await expect(textProductPage).toBeDisplayed();
     });
 })
